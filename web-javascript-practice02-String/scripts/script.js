28e0683e3177add931a35fbd8bdb1886f390034a
felix <felixzxy29@gmail.com>
2024-05-14 16:27:58 +0800

    practiced javascript String

---
+++ web-javascript-practice02-String/scripts/script.js
@@ -0,0 +1,39 @@
+let name = "       Felix Zeng        ";
+
+// alert(name.repeat(2));
+// alert(name.repeat(4));
+// alert(name);
+// alert(name.replace("miraitowa", "felIx"));
+// alert(name.replace(/i/gi, function(ele){
+//     return ele.toUpperCase();
+// }));
+// alert(name.search(/blue/i));
+// alert(name.slice(0));
+// alert(name.slice(-5));
+// alert(name.slice(3));
+// alert(name.slice(0, 5));
+// alert(name.slice(4,8));
+
+// let res = name.split(" ");
+// for (let i = 0; i < res.length; i++) {
+//     alert(res[i]);
+// }
+// console.log(res);
+//
+// let res2 = name.split(" ", 3);
+// console.log(res2);
+
+// alert(name.startsWith(" ", 3));
+// alert(name.substr(4, 4));
+// alert(name.substr(-1));
+// alert(name.substring(0, name.length - 1));
+// alert(name.substring(-1, 3));
+// alert(name.substring(name.length -1));
+
+// alert(name.toUpperCase());
+// alert(name.toLowerCase());
+// alert(name.toString());
+// alert(name.trim());
+// alert(name.trimEnd());
+// alert(name.trimStart());
+alert(name.valueOf());
