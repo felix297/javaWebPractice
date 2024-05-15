9809b6d323eb65e939294dce70b66b4b6d08e042
felix <felixzxy29@gmail.com>
2024-05-15 22:55:24 +0800

    practice Array

--- web-javascript-practice04-Array/scripts/script.js
+++ web-javascript-practice04-Array/scripts/script.js
@@ -1,5 +1,5 @@
-let arr = [1, 2, 3, 4, 5, 6];
-let arr2 = [7, 8, 9];
+let arr = [18, 58, 20, 39, 18];
+let fruit = ["apple", "banana", "pineapple"];
 let arr3 = [10, 11, 12];
 
 // for (let i = 0; i < arr.length; i++) {
@@ -24,8 +24,57 @@ let arr3 = [10, 11, 12];
 //     console.log(arr[i]);
 // }
 
-let iterator = arr.entries();
+// let iterator = arr.entries();
+//
+// for (ele of iterator) {
+//     console.log(ele);
+// }
 
-for (ele of iterator) {
+// function lt18 (age) {
+//     return age >= 18;
+// }
+
+// function equal (age, index, arr) {
+//     if (index === 0) {
+//         return true;
+//     } else {
+//         return age === arr[index - 1]
+//     }
+// }
+//
+// alert(arr.every(equal));
+
+// arr.fill(1, 2, 5);
+//
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// function over18 (age) {
+//     return age > 18;
+// }
+//
+// alert(arr.filter(over18));
+
+// function over18 (age) {
+//     return age > 18;
+// }
+//
+// alert(arr.find(over18));
+
+// function over18 (age) {
+//     return age > 18;
+// }
+//
+// alert(arr.findIndex(over18));
+
+function times (ele, index) {
+    arr[index] = arr[index] * 10;
+}
+
+function print(ele) {
     console.log(ele);
 }
+
+arr.forEach(times);
+arr.forEach(print);
