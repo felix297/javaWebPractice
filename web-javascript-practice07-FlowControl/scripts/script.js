647d68ba52cff41de23e1da5552b51fb81bd47a4
felix <felixzxy29@gmail.com>
2024-05-17 13:02:48 +0800

    practice flow control

---
+++ web-javascript-practice07-FlowControl/scripts/script.js
@@ -0,0 +1,43 @@
+// let age = 18;
+// let score = 90;
+//
+// if (age > 18) {
+//     alert("you're an adult!");
+// } else {
+//     alert("you are not an adult! please waiting until you are an adult!");
+// }
+//
+// if (score > 80) {
+//     alert("excellent");
+// } else if (score > 60) {
+//     alert("not bad");
+// } else {
+//     alert("unqualified");
+// }
+
+// let i = 0;
+// let sum = 0;
+// while (i < 10) {
+//     sum += i;
+//     i++;
+// }
+// alert(sum);
+
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+//     sum += i;
+// }
+// alert(sum);
+
+// let nums = [1, 2,3, 4,5,6,7,8, 9 , 10];
+// function sum (ele) {
+//     console.log(ele* 10);
+// }
+// nums.forEach(sum);
+// for (ele of nums.values()) {
+//     console.log(ele);
+// }
+//
+// for (ele in nums) {
+//     console.log(nums[ele]);
+// }
