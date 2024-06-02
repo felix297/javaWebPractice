65b25e05dec8ea4d80873256f7f72acbec2dbf8a
felix <felixzxy29@gmail.com>
2024-06-02 16:22:11 +0800

    practice window

---
+++ web-javascript-practice12-BOM/scripts/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// window.alert("hello");
+// window.alert("innerWidth: " + window.innerWidth);
+// window.alert("innerHeight: " + window.innerHeight);
+
+// window.open();
+// window.close();
