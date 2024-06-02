d5487549d531d0e2dc1ee6a0e8a1a1ca37f8211d
felix <felixzxy29@gmail.com>
2024-06-02 16:50:59 +0800

    practice window.history

--- web-javascript-practice12-BOM/scripts/script.js
+++ web-javascript-practice12-BOM/scripts/script.js
@@ -14,9 +14,12 @@
 // window.alert("color depth: " + screen.colorDepth);
 // window.alert("pixel depth: " + screen.pixelDepth);
 
-window.console.log("url: " + window.location.href);
-window.console.log("hostname: " + window.location.hostname);
-window.console.log("port: " + window.location.port);
-window.console.log("path: " + window.location.pathname);
-window.console.log("protocol: " + window.location.protocol);
-window.location.assign("https://www.baidu.com");
+// window.console.log("url: " + window.location.href);
+// window.console.log("hostname: " + window.location.hostname);
+// window.console.log("port: " + window.location.port);
+// window.console.log("path: " + window.location.pathname);
+// window.console.log("protocol: " + window.location.protocol);
+// window.location.assign("https://www.baidu.com");
+//
+// window.history.back()
+// window.history.forward();
