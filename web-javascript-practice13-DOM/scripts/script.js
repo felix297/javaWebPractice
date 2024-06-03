d0b96a3ba3bfb1f28b42b035166bb0334c89c61e
felix <felixzxy29@gmail.com>
2024-06-03 12:16:19 +0800

    practice dom find element

---
+++ web-javascript-practice13-DOM/scripts/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// window.document.getElementById("firstP").innerText="你好！";
+// window.console.log(window.document.getElementById("firstP").innerText);
+
+// window.document.getElementsByTagName("p")[0].innerText = "!";
+
+// window.document.getElementsByClassName("h1")[0].innerText = "??????";
+
+// window.document.querySelectorAll("#firstP.h1")[0].innerHTML = "querySelectorAll";
+// window.document.querySelector("#firstP.h1").innerHTML = "querySelector";
+
+// window.document.anchors["a1"].innerText = "a1";
+// let arr = [1,2,3,4,5];
+// alert(typeof arr);
+// alert(typeof window.document.anchors);
+// alert(document.baseURI);
+// alert(document.anchors["top"].innerText)
+
