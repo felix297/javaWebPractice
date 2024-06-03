e1ba723e97ede08e611340bbcbdc3e558eedb89a
felix <felixzxy29@gmail.com>
2024-06-03 23:04:53 +0800

    practice jQuery selector

--- web-javascript-practice14-jQuery/scripts/script.js
+++ web-javascript-practice14-jQuery/scripts/script.js
@@ -9,3 +9,16 @@
 // $(document).ready(function() {
 //     $("#p").hide();
 // });
+
+// $(document).ready(function () {
+//     console.log($("p#p").text());
+//     $("[id]").hide();
+// });
+
+// $(document).ready(function() {
+//     $("#p").css("color", "red");
+// });
+
+$(document).ready(function () {
+    $(".p").css("color", "red");
+});
