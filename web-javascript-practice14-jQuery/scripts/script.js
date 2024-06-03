180efd01abeeff4c2967f6cf53de48ce509c9fc2
felix <felixzxy29@gmail.com>
2024-06-03 23:43:43 +0800

    practice jQuery hide and show

--- web-javascript-practice14-jQuery/scripts/script.js
+++ web-javascript-practice14-jQuery/scripts/script.js
@@ -19,6 +19,39 @@
 //     $("#p").css("color", "red");
 // });
 
-$(document).ready(function () {
-    $(".p").css("color", "red");
-});
+// $(document).ready(function () {
+//     $(".p").css("color", "red");
+// });
+
+// $(document).ready(function () {
+//     $("#p").click(function () {
+//         $("#p").hide();
+//     });
+// });
+//
+// $(document).ready(function () {
+//     $("#p2").dblclick(function() {
+//         $("#p2").hide();
+//     });
+// });
+
+// $(document).ready(function() {
+//     $("#p").mouseover(function() {
+//         $("#p").hide();
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("#show").click(function () {
+//         $("h1").show(100);
+//     });
+//
+//     $("#hide").click(function () {
+//         $("h1").hide(100);
+//     });
+//
+//     $("#toggle").click(function(){
+//         $("h1").toggle(1000);
+//     });
+// });
+
