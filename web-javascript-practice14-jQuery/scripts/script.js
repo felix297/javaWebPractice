f73afbfd74a5e13a1e45f1fd196b51367e870a23
felix <felixzxy29@gmail.com>
2024-06-03 22:39:22 +0800

    practice jQuery demo

---
+++ web-javascript-practice14-jQuery/scripts/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// $(document).ready(function() {
+//     let ele = $("#p");
+//     console.log(ele.text());
+// });
+//
+
+// $(document).ready(function() {
+//     $("#p").hide();
+// });
