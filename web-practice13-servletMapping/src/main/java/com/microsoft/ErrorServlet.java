604cc61b3ae9df751b29a4f43c8447ba8821b2c7
felix <felixzxy29@gmail.com>
2024-06-11 13:08:16 +0800

    practice servlet mapping

--- web-practice13-servletMapping/src/main/java/com/microsoft/ErrorServlet.java
+++ web-practice13-servletMapping/src/main/java/com/microsoft/ErrorServlet.java
@@ -1,2 +1,41 @@
-package com.microsoft;public class ErrorServlet {
+package com.microsoft;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ErrorServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("<html lang='en'>");
+            out.println("<head>");
+            out.println("<title>");
+            out.println("error page");
+            out.println("</title>");
+            out.println("<meta charset='UTF-8'/>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1 color='red' font-family='consolas, sans-serif'>");
+            out.println("Page Not Found");
+            out.println("</h1>");
+            out.println("</body>");
+            out.println("<html>");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        doGet(request, response);
+    }
 }
--- web-practice13-servletMapping/src/main/java/com/microsoft/ServletMappingTest.java
+++ web-practice13-servletMapping/src/main/java/com/microsoft/ServletMappingTest.java
@@ -1,2 +1,39 @@
-package com.microsoft;public class ServletMappingTest {
+package com.microsoft;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletMappingTest extends HttpServlet{
+    @Override
+    public void doGet (HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("<html lang='en'>");
+            out.println("<head>");
+            out.println("<title>servlet</title>");
+            out.println("<meta charset='UTF-8'/>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>");
+            out.println("Hello!");
+            out.println("</h1>");
+            out.println("</body>");
+            out.println("</html>");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void doPost (HttpServletRequest request, HttpServletResponse response) {
+        doGet(request, response);
+    }
 }
