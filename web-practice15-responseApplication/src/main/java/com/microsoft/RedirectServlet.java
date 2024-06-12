321ac96dc7aa6cbde43ebeefda717a349e34b557
felix <felixzxy29@gmail.com>
2024-06-12 20:52:11 +0800

    practice redirect

---
+++ web-practice15-responseApplication/src/main/java/com/microsoft/RedirectServlet.java
@@ -0,0 +1,24 @@
+package com.microsoft;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+
+public class RedirectServlet extends HttpServlet {
+    @Override
+    public void doGet (HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("RedirectServlet");
+        try {
+            response.sendRedirect("/practice15/error.html");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void doPost (HttpServletRequest request, HttpServletResponse response) {
+        doGet(request, response);
+    }
+
+}
