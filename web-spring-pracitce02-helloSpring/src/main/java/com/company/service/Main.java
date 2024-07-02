b6ca8d19d2b98e209b6f49c0449eb9f1080b1a86
felix <felixzxy29@gmail.com>
2024-07-02 14:51:49 +0800

    practice bean import

--- web-spring-pracitce02-helloSpring/src/main/java/com/company/service/Main.java
+++ web-spring-pracitce02-helloSpring/src/main/java/com/company/service/Main.java
@@ -18,23 +18,25 @@ public class Main {
 //        Hello hello = (Hello)applicationContext.getBean("hello");
 //        System.out.println(hello.getMsg());
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Hello hello = (Hello)applicationContext.getBean("hello");
-        hello.setMsg("Hello! Spring! -- by bean-id 'hello' ");
-        System.out.println(hello.getMsg());
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+//        Hello hello = (Hello)applicationContext.getBean("hello");
+//        hello.setMsg("Hello! Spring! -- by bean-id 'hello' ");
+//        System.out.println(hello.getMsg());
+//
+//        Hello hello2 = (Hello) applicationContext.getBean("hello2");
+//        hello2.setMsg("Hello! Spring! -- by bean-name 'hello2'");
+//        System.out.println(hello2.getMsg());
+//
+//        Hello hello3 = (Hello)applicationContext.getBean("hello3");
+//        hello3.setMsg("Hello! Spring! -- by alias 'hello3'");
+//        System.out.println(hello3.getMsg());
+//
+//        Hello hello4 = (Hello) applicationContext.getBean("hello4");
+//        hello4.setMsg("Hello! Spring! -- by bean-name 'hello4'");
+//        System.out.println(hello4.getMsg());
 
-        Hello hello2 = (Hello) applicationContext.getBean("hello2");
-        hello2.setMsg("Hello! Spring! -- by bean-name 'hello2'");
-        System.out.println(hello2.getMsg());
-
-        Hello hello3 = (Hello)applicationContext.getBean("hello3");
-        hello3.setMsg("Hello! Spring! -- by alias 'hello3'");
-        System.out.println(hello3.getMsg());
-
-        Hello hello4 = (Hello) applicationContext.getBean("hello4");
-        hello4.setMsg("Hello! Spring! -- by bean-name 'hello4'");
-        System.out.println(hello4.getMsg());
-
-
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans2.xml");
+        Service service = (Service) applicationContext.getBean("service");
+        service.printMsg();
     }
 }
