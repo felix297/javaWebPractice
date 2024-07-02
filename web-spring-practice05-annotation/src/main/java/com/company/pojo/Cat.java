4a487baca9496ee1b564f950c16ffeb77e9bf28f
felix <felixzxy29@gmail.com>
2024-07-02 21:36:25 +0800

    practice use the annotation to development

---
+++ web-spring-practice05-annotation/src/main/java/com/company/pojo/Cat.java
@@ -0,0 +1,36 @@
+package com.company.pojo;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope("singleton")
+//@Scope("prototype")
+public class Cat {
+    @Value("tom")
+    private String name;
+
+    @Value("male")
+    private String gender;
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setGender (String gender) {
+        this.gender = gender;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public String getGender () {
+        return this.gender;
+    }
+
+    public String toString () {
+        return "name: " + name + " gender: " + gender;
+    }
+}
---
+++ web-spring-practice05-annotation/src/main/java/com/company/service/Service.java
@@ -0,0 +1,14 @@
+package com.company.service;
+
+import com.company.pojo.Cat;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Service {
+    public static void main (String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat1.hashCode() == cat2.hashCode());
+    }
+}
